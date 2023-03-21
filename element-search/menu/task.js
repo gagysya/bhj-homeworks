0cd1095dcfff72d475d3c5003f428aa9eb108336
gagysya <gagysya@yandex.ru>
2023-03-21 14:28:28 +0300

    Update task.js
    git commit -m 'first commit menu'

---
+++ element-search/menu/task.js
@@ -0,0 +1,14 @@
+const links = Array.from(document.querySelectorAll(".menu__link"));
+const subMenu = Array.from(document.querySelectorAll(".menu_sub"));
+
+links.forEach((link) => {
+  link.onclick = () => {
+    if (link.parentElement.querySelector(".menu_sub") !== null) {
+      subMenu.forEach((item) => item.classList.remove("menu_active"));
+      link.parentElement
+        .querySelector(".menu_sub")
+        .classList.add("menu_active");
+      return false;
+    }
+  };
+});
